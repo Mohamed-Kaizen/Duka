c5fa21efd043ea3b4076fbf4001bcced994650b5
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2021-11-15 16:48:08 +0300

    update migrations files

---
+++ hasura/seeds/default/1636980400591_pickup_data.sql
@@ -0,0 +1,2 @@
+SET check_function_bodies = false;
+INSERT INTO public.pickup (id, name, price, is_active, created_at, updated_at) VALUES ('2458bcc7-e90b-4d48-8dbf-cc335cf2a935', 'default', 400, true, '2021-11-15 12:46:10.156667+00', '2021-11-15 12:46:10.156667+00');
